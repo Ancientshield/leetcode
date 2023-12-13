263fd2f54fee351c6b70c61cbcf2a37cb3b49476
Jack Lin <jack_g1341@goodwill.coml.tw>
2023-12-13 16:49:24 +0800

    modify 015 2725. Interval Cancellation

--- 2725. Interval Cancellation_015.js
+++ 2725. Interval Cancellation_015.js
@@ -1,5 +1,12 @@
 const cancellable = (fn, args, t) => {
+	// 第一次執行
 	fn(...args);
 	const timer = setInterval(() => fn(...args), t);
-	return () => clearInterval(timer);
+
+	const cancelFn = () => {
+		clearInterval(timer);
+	};
+	return cancelFn;
+	// 也可直接 return function，但依題意，還是命名 cancelFn
+	// return () => clearInterval(timer);
 };
