12e087d22f79fb2923e89bddf8e03b2af3a9c751
Jack Lin <jack_g1341@goodwill.coml.tw>
2023-12-13 17:10:11 +0800

    modify 016 2637. Promise Time Limit

--- 2637. Promise Time Limit_016.js
+++ 2637. Promise Time Limit_016.js
@@ -1,10 +1,11 @@
 const timeLimit = (fn, t) => {
 	return (...args) => {
+		// 執行內部函式 並回傳結果 resolve 或 reject
 		return new Promise((resolve, reject) => {
+			//這兩個程式是同時進行的，誰先完成就會先解析 Promise
 			setTimeout(() => {
 				reject('Time Limit Exceeded');
 			}, t);
-
 			fn(...args).then(resolve, reject);
 		});
 	};
