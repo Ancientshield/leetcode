2b0830f06b6838315d6b57a1aeb9f2cbea7bcddb
Jack Lin <jack_g1341@goodwill.coml.tw>
2023-12-14 15:11:18 +0800

    finished 019 2721. Execute Asynchronous Functions in Parallel leetcode solution

---
+++ 2721. Execute Asynchronous Functions in Parallel leetcode solution_019.js
@@ -0,0 +1,24 @@
+const promiseAll = (functions) => {
+	return new Promise((resolve, reject) => {
+		const results = [];
+		let resolvedCount = 0;
+
+		//解析functions數組的邏輯
+		for (let i = 0; i < functions.length; i++) {
+			// 使用  IIFE（Immediately Invoked Function Expression）傳遞 i 值
+			(() => {
+				functions[i]()
+					.then((result) => {
+						results[i] = result;
+						resolvedCount++;
+						if (resolvedCount === functions.length) {
+							resolve(results);
+						}
+					})
+					.catch((error) => {
+						reject(error);
+					});
+			})(i);
+		}
+	});
+};
