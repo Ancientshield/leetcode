e8e17bc4d38f2a7e9154b12a2309fe51e7ea195a
Jack Lin <jack_g1341@goodwill.coml.tw>
2023-12-14 17:28:21 +0800

    modify 023 2631. Group By

--- 2631. Group By_023.js
+++ 2631. Group By_023.js
@@ -1,12 +1,19 @@
 Array.prototype.groupBy = function (fn) {
+	// 創建一個空物件來儲存分組後的元素
 	const group = {};
-	// 分組邏輯
+
+	// 遍歷陣列中的每個 e 元素(element)
 	for (let e of this) {
+		// 使用提供的回調函數(fn)獲取當前元素的鍵值
 		const key = fn(e);
-		// 使用 object[key] 訪問屬性，如果不存在，則group[key] 賦值為空陣列 []
+
+		// 如果在 group 物件中該鍵不存在，則為該鍵創建一個空陣列
 		group[key] || (group[key] = []);
-		// 把原始陣列元素 push 到對應的 grouped[key] 內
+
+		// 將當前元素推送到與其鍵值對應的陣列中
 		group[key].push(e);
 	}
+
+	// 回傳最終的分組物件
 	return group;
 };
